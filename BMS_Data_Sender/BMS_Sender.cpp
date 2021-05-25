089856ed9b19c0b77eff5dfd6e331ddaaace63eb
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-05-25 20:31:52 +0530

    Create BMS_Sender.cpp

---
+++ BMS_Data_Sender/BMS_Sender.cpp
@@ -0,0 +1,6 @@
+/* ***********************************************************************************************************
+* File Name   :	BMS_Sender.cpp
+* Author      : Arul Kumar Sundaramoorthy
+* Description : BMS_data sender.cpp is used to send the BMS datas like temperature and State of Charging
+* Functions	  :
+* *********************************************************************************************************** */

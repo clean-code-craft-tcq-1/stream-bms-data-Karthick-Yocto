501983fbcd83aeb82670cb631b02cd3c06c71e43
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-05-25 21:47:41 +0530

    Update BMS_Sender.cpp

--- BMS_Data_Sender/BMS_Sender.cpp
+++ BMS_Data_Sender/BMS_Sender.cpp
@@ -4,3 +4,7 @@
 * Description : BMS_data sender.cpp is used to send the BMS datas like temperature and State of Charging
 * Functions	  :
 * *********************************************************************************************************** */
+
+main()
+{
+}

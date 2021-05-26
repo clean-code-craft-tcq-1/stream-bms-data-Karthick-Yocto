5df76b294070eadde15ec4a8005664db2110ef79
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-05-26 19:53:25 +0530

    Update BMS_Sender.h

--- BMS_Data_Sender/BMS_Sender.h
+++ BMS_Data_Sender/BMS_Sender.h
@@ -1,1 +1,14 @@
+#pragma once
+
+class BMSParameter
+{
+public:
+    bool inRange(int minValueChecker, int maxValueChecker);
+    float BmsSender_ReadTemperture();
+    float BmsSender_ReadSOC();
+    bool printOnConsole(std::string parameterType, int GenerateNumbers);
+
+};
+
+
 

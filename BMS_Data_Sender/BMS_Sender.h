575f7d99c11932fcff7599c64d172b23d0dcd24a
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-05-26 20:27:35 +0530

    Update BMS_Sender.h

--- BMS_Data_Sender/BMS_Sender.h
+++ BMS_Data_Sender/BMS_Sender.h
@@ -1,20 +1,21 @@
 #pragma once
+
+//Temperature
 #define MIN_TEMP    -25.0
 #define MAX_TEMP    100.0
 
-#define ERROR_TEMP  150.0
-
+//SOC
 #define MIN_SOC     0.0
 #define MAX_SOC     100.0
 
-#define ERROR_SOC   255.0
+
 
 class BMSParameter
 {
 public:
     bool inRange(int minValueChecker, int maxValueChecker);
-    float BmsSender_ReadTemperture();
-    float BmsSender_ReadSOC();
+    float BmsSender_Temperture();
+    float BmsSender_SOC();
     bool printOnConsole(std::string parameterType, int GenerateNumbers);
 
 };

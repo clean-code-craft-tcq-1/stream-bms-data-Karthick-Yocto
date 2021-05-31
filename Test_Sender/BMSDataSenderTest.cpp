7000d5a4f6a50d6b5882dedf56165e8c83bef1f2
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-05-31 15:45:20 +0530

    Update BMSDataSenderTest.cpp

--- Test_Sender/BMSDataSenderTest.cpp
+++ Test_Sender/BMSDataSenderTest.cpp
@@ -11,8 +11,9 @@ TEST_CASE("BMS Data Sender Test")
     for (int iter = 0; iter < 50; iter++) 
     {
 	    float temperature =sender_obj->BmsSender_Temperture();   
-      float SOC= sender_obj->BmsSender_SOC();
-      
+      	    float SOC= sender_obj->BmsSender_SOC();
+      	    std::cout<<"Temperature="<<temperature<<endl;
+	    std::cout<<"SOC="<<SOC<<endl;
       
 	    REQUIRE(sender_obj->inRange(temperature, MIN_SOC, MAX_SOC));
 	    REQUIRE(sender_obj->inRange(SOC, MIN_TEMP, MAX_TEMP));

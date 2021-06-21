90cdb089f298bbc14346aa72473ad24d369f6428
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-06-21 09:37:05 +0530

    Update Program.cs

--- BMS_DataStream_Receiver/BMS_DataStream_Receiver/Program.cs
+++ BMS_DataStream_Receiver/BMS_DataStream_Receiver/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,31 +13,28 @@ namespace BMS_DataStream_Receiver
         {
             IDataStreamReceiver receiver = new DataStreamReceiver();
             IDataProcessor processor = new DataProcessor(receiver);
-            List<string> batteryData = new List<string>();
-            string ReceivedData= GenerateRandomNumber();
-            
+            List<string> MovingDataList = new List<string>();
+          
             int i = 0;
-            Console.WriteLine(ReceivedData);
-            var inputstream = ReceivedData.Split('\n');
+        
             string value = string.Empty;
                while ((value = Console.ReadLine()) != null)
-           // foreach (var value in inputstream)
 
             {
                 if (value != string.Empty && value!=null)
                 {
                     i++;
                     OutputParameter OutputParam;
-                    batteryData.Add(value);
+                    MovingDataList.Add(value);
                     OutputParam = processor.GetMinimumAndMaximumValues(value);
                     Console.WriteLine("Minimum Temperature:{0}\tMaximum Temperature:{1}\tMinimum StateOfCharge:{2}\tMaximum StateOfCharge:{3}", OutputParam.Temperature.Minimum, OutputParam.Temperature.Maximum, OutputParam.StateOfCharge.Minimum, OutputParam.StateOfCharge.Maximum);
                     if (i == 5)
                     {
 
-                        OutputParam = processor.GetMovingAverageValue(batteryData);
+                        OutputParam = processor.GetMovingAverageValue(MovingDataList);
                         Console.WriteLine("Temperature Moving Avg.:{0}\n SOC Moving Avg.:{1}\n", OutputParam.Temperature.MovingAverage, OutputParam.StateOfCharge.MovingAverage);
 
-                        batteryData = new List<string>();
+                        MovingDataList = new List<string>();
                         i = 0;
                     }
                 }
